1c3be975c30530911be58d69931f2c96b76fd348
Phoebe Zhang <zphoebe@outlook.com>
2023-03-15 15:51:20 +1100

    Refactored shared prefs save/restore

--- app/src/main/java/com/fit2081/bookstoreapp/MainActivity.java
+++ app/src/main/java/com/fit2081/bookstoreapp/MainActivity.java
@@ -1,40 +1,76 @@
 package com.fit2081.bookstoreapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.jetbrains.annotations.NonNls;
+
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String KEY_BOOK_ID = "book_id";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_ISBN = "isbn";
+    public static final String KEY_AUTHOR = "author";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_PRICE = "price";
+    private EditText etBookId;
+    private EditText etTitle;
+    private EditText etIsbn;
+    private EditText etAuthor;
+    private EditText etDescription;
+    private EditText etPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etBookId = findViewById(R.id.book_id);
+        etTitle = findViewById(R.id.title_id);
+        etIsbn = findViewById(R.id.isbn_id);
+        etAuthor = findViewById(R.id.author_id);
+        etDescription = findViewById(R.id.description_id);
+        etPrice = findViewById(R.id.price_id);
+
         if (savedInstanceState == null) {
-            onLoadBookBtnClick(null);
+            SharedPreferences prefs = getSharedPreferences("book", MODE_PRIVATE);
+            etBookId.setText(prefs.getString(KEY_BOOK_ID, ""));
+            etTitle.setText(prefs.getString(KEY_TITLE, ""));
+            etIsbn.setText(prefs.getString(KEY_ISBN, ""));
+            etAuthor.setText(prefs.getString(KEY_AUTHOR, ""));
+            etDescription.setText(prefs.getString(KEY_DESCRIPTION, ""));
+            etPrice.setText(prefs.getString(KEY_PRICE, ""));
         }
     }
 
-    public void onAddBookBtnClick(View view) {
-        // get ref to EditTexts
-        EditText bookIdEt = findViewById(R.id.book_id);
-        EditText titleEt = findViewById(R.id.title_id);
-        EditText isbnEt = findViewById(R.id.isbn_id);
-        EditText authorEt = findViewById(R.id.author_id);
-        EditText descriptionEt = findViewById(R.id.description_id);
-        EditText priceEt = findViewById(R.id.price_id);
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putString(KEY_TITLE, etTitle.getText().toString());
+        outState.putString(KEY_ISBN, etIsbn.getText().toString());
+    }
 
-        // get attributes for toast
-        String title = titleEt.getText().toString();
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        etTitle.setText(savedInstanceState.getString(KEY_TITLE));
+        etIsbn.setText(savedInstanceState.getString(KEY_ISBN));
+    }
+
+    public void onAddBookBtnClick(View view) {
+        // get title and price
+        String title = etTitle.getText().toString();
         float price = 0;
-        if (!priceEt.getText().toString().isEmpty()) {
-            price = Float.parseFloat(priceEt.getText().toString());
+        if (!etPrice.getText().toString().isEmpty()) {
+            price = Float.parseFloat(etPrice.getText().toString());
         }
 
         // display toast
@@ -44,39 +80,31 @@ public class MainActivity extends AppCompatActivity {
         // save attributes
         SharedPreferences prefs = getSharedPreferences("book", MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
-        editor.putString("book_id", bookIdEt.getText().toString());
-        editor.putString("title", title);
-        editor.putString("isbn", isbnEt.getText().toString());
-        editor.putString("author", authorEt.getText().toString());
-        editor.putString("description", descriptionEt.getText().toString());
-        editor.putFloat("price", price);
+        editor.putString(KEY_BOOK_ID, etBookId.getText().toString());
+        editor.putString(KEY_TITLE, title);
+        editor.putString(KEY_ISBN, etIsbn.getText().toString());
+        editor.putString(KEY_AUTHOR, etAuthor.getText().toString());
+        editor.putString(KEY_DESCRIPTION, etDescription.getText().toString());
+        editor.putString(KEY_PRICE, etPrice.getText().toString());
         editor.apply();
     }
 
     public void onClearFieldsBtnClick(View view) {
-        ((EditText) findViewById(R.id.book_id)).setText("");
-        ((EditText) findViewById(R.id.title_id)).setText("");
-        ((EditText) findViewById(R.id.isbn_id)).setText("");
-        ((EditText) findViewById(R.id.author_id)).setText("");
-        ((EditText) findViewById(R.id.description_id)).setText("");
-        ((EditText) findViewById(R.id.price_id)).setText("");
+        etBookId.setText("");
+        etTitle.setText("");
+        etIsbn.setText("");
+        etAuthor.setText("");
+        etDescription.setText("");
+        etPrice.setText("");
     }
 
     public void onLoadBookBtnClick(View view) {
         SharedPreferences prefs = getSharedPreferences("book", MODE_PRIVATE);
-        ((EditText) findViewById(R.id.book_id)).setText(prefs.getString("book_id", ""));
-        ((EditText) findViewById(R.id.title_id)).setText(prefs.getString("title", ""));
-        ((EditText) findViewById(R.id.isbn_id)).setText(prefs.getString("isbn", ""));
-        ((EditText) findViewById(R.id.author_id)).setText(prefs.getString("author", ""));
-        ((EditText) findViewById(R.id.description_id)).setText(prefs.getString("description", ""));
-
-        float price = prefs.getFloat("price", 0);
-        if (price == 0) {
-            ((EditText) findViewById(R.id.price_id)).setText("");
-        } else if (price == Math.round(price)) {
-            ((EditText) findViewById(R.id.price_id)).setText(String.valueOf((int) price));
-        } else {
-            ((EditText) findViewById(R.id.price_id)).setText(String.valueOf(price));
-        }
+        etBookId.setText(prefs.getString(KEY_BOOK_ID, ""));
+        etTitle.setText(prefs.getString(KEY_TITLE, ""));
+        etIsbn.setText(prefs.getString(KEY_ISBN, ""));
+        etAuthor.setText(prefs.getString(KEY_AUTHOR, ""));
+        etDescription.setText(prefs.getString(KEY_DESCRIPTION, ""));
+        etPrice.setText(prefs.getString(KEY_PRICE, ""));
     }
 }
